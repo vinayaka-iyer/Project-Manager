aa96088d48cb3634d00c4323772dec0641266200
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-14 18:48:26 +0530

    Signup User + uploading Profile Thumbnail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import Temple from '../assets/temple.svg'
+import Workshop from '../assets/workshop.svg'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
 		<div className='navbar'>
 			<ul>
 				<li className='logo'>
-					<img src={Temple} alt='workshop logo' />
+					<img src={Workshop} alt='workshop logo' />
 					<span>The Workshop</span>
 				</li>
 
@@ -16,7 +16,7 @@ const Navbar = () => {
 					<Link to='/login'>Login</Link>
 				</li>
 				<li>
-					<Link to='/singup'>Signup</Link>
+					<Link to='/signup'>Signup</Link>
 				</li>
 				<li>
 					<button className='btn'>Logout</button>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBAs32actBVDzme6V8nIwYvPAEPm_xXTAA",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export {projectFirestore, projectAuth, timestamp}
+export {projectFirestore, projectAuth, projectStorage, timestamp}
