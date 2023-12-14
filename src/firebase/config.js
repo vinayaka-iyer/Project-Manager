0c0a75b54d504a208547095bdd9f9bfc2e8807a7
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-14 15:54:33 +0530

    Project setup

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from '/firebas/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBAs32actBVDzme6V8nIwYvPAEPm_xXTAA",
+	authDomain: "ven-s-workshop.firebaseapp.com",
+	projectId: "ven-s-workshop",
+	storageBucket: "ven-s-workshop.appspot.com",
+	messagingSenderId: "318990754781",
+	appId: "1:318990754781:web:52ef76ad04c41a6de1cc34",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export {projectFirestore, projectAuth, timestamp}
