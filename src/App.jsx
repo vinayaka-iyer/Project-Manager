4a02765224287496ad130c8cbc9bfb5483fbf89a
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-14 17:13:51 +0530

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+// pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Project from "./pages/project/Project";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
-
-  return (
-    <div>
-
-    </div>
-  )
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<div className='container'>
+					<Switch>
+						<Route exact path='/'>
+							<Dashboard />
+						</Route>
+						<Route path='/create'>
+							<Create />
+						</Route>
+						<Route path='/login'>
+							<Login />
+						</Route>
+						<Route path='/signup'>
+							<Signup />
+						</Route>
+						<Route path='/projects/:id'>
+							<Project />
+						</Route>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
