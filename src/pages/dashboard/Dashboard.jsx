50f8c9bc465685fd8f25f50d90f7a2827567f74f
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-15 15:15:46 +0530

    Project list component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Dashboard.css";
+import { useCollection } from "../../hooks/useCollection";
+import ProjectList from "../../components/ProjectList";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const {documents, error} = useCollection('projects')
+	return (
+		<div>
+			<h2 className='page-title'>Dashboard</h2>
+			{error && <p className='error'>{error}</p>}
+			{documents && <ProjectList projects={documents} />}
+		</div>
+	);
 };
 
 export default Dashboard;
