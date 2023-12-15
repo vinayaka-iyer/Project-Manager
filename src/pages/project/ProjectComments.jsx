561f979cbea385df9dfaa8f871bafebffc575470
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2023-12-15 18:50:12 +0530

    Firestore rules + deploy

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -3,6 +3,7 @@ import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
 import Avatar from "../../components/Avatar";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const ProjectComments = ({project}) => {
     const {updateDocument, response} = useFirestore('projects')
@@ -38,7 +39,7 @@ const ProjectComments = ({project}) => {
 								<p>{comment.displayName}</p>
 							</div>
 							<div className='comment-date'>
-								<p>date here</p>
+								<p>{formatDistanceToNow(comment.createdAt.toDate(), {addSuffix: true})}</p>
 							</div>
                             <div className="comment-content">
                                 <p>{comment.content}</p>
